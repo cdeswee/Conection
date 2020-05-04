ccef3f52f2d9caff785fdd89507c68eea150b437
Boris Milasinovic <boris.milasinovic@fer.hr>
2020-05-04 14:51:10 +0200

    Added Npgsql package and fixed form code

--- Conexion/Conexion/Form1.cs
+++ Conexion/Conexion/Form1.cs
@@ -1,57 +1,60 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Windows.Forms;
 
 namespace Conexion
 {
-    public partial class Form1 : Form
+  public partial class Form1 : Form
+  {
+
+    string cadenaConexion;
+    public Form1()
     {
-        NpgsqlConnection conexión = new NpgsqlConnectionNpgsqlConnection();
-        string cadenaConexion;
-        public Form1()
-        {
-            InitializeComponent();
-            cadenaConexion = " Server =  127.0.0.1; Port=5432; Database=parkboat;";
-            cadenaConexion += " User Id =admin; "; " 
-            cadenaConexion += "; Password = lavall2c";
-            conexion.ConnectionString = cadenaConexion;
-            try
-            {
-                conexion.Open();
-            }
-            catch
-            {
-                MessageBox.Show ("User or parssword incorrect");
-                conexion.Close();
-            }
-            if(conexion.state.ToString()=="open")
-                {
-                MessageBox.Show("Connection relized succesfuly");
-            }
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
+      InitializeComponent();            
     }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label5_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label3_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      using (NpgsqlConnection conexion = new NpgsqlConnection())
+      {
+        cadenaConexion = " Server =  127.0.0.1; Port=5432; Database=parkboat;";
+        cadenaConexion += " User Id =admin; ";
+            cadenaConexion += "; Password = lavall2c";
+        conexion.ConnectionString = cadenaConexion;
+        try
+        {
+          conexion.Open();
+        }
+        catch
+        {
+          MessageBox.Show("User or parssword incorrect");
+          conexion.Close();
+        }
+        if (conexion.State.ToString() == "open")
+        {
+          MessageBox.Show("Connection relized succesfuly");
+        }
+      }
+    }
+  }
 }
