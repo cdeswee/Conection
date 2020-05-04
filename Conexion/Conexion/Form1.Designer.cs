784b3222d9c7f4d74268f8bb77ae74044d92205a
Claudia De Sweemer <claudiadesweemer@gmail.com>
2020-05-04 18:37:22 +0200

    Inserction and display information

--- Conexion/Conexion/Form1.Designer.cs
+++ Conexion/Conexion/Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonAlta = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -36,26 +36,27 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonAlta
             // 
-            this.button1.Location = new System.Drawing.Point(190, 336);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(174, 45);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Dar de alta";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonAlta.Location = new System.Drawing.Point(190, 336);
+            this.buttonAlta.Name = "buttonAlta";
+            this.buttonAlta.Size = new System.Drawing.Size(174, 45);
+            this.buttonAlta.TabIndex = 0;
+            this.buttonAlta.Text = "Dar de alta";
+            this.buttonAlta.UseVisualStyleBackColor = true;
+            this.buttonAlta.Click += new System.EventHandler(this.buttonAlta_Click);
             // 
             // button2
             // 
@@ -65,15 +66,16 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Mostrar registro";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(114, 63);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.Size = new System.Drawing.Size(15, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "ID";
+            this.label1.Text = "id";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
@@ -81,18 +83,18 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(92, 103);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Username";
+            this.label2.Text = "username";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(92, 135);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.Size = new System.Drawing.Size(52, 13);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Password";
+            this.label3.Text = "password";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
@@ -100,18 +102,18 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(101, 165);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.Size = new System.Drawing.Size(31, 13);
             this.label4.TabIndex = 5;
-            this.label4.Text = "Email";
+            this.label4.Text = "email";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(92, 200);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.Size = new System.Drawing.Size(72, 13);
             this.label5.TabIndex = 6;
-            this.label5.Text = "Phonenumber";
+            this.label5.Text = "phonenumber";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // listView1
@@ -129,6 +131,26 @@
             this.listView1.TabIndex = 7;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "id";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "username";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "password";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "email";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "phonenumber";
             // 
             // textBox1
             // 
@@ -166,31 +188,11 @@
             this.textBox5.Size = new System.Drawing.Size(120, 20);
             this.textBox5.TabIndex = 12;
             // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "ID";
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "Username";
-            // 
-            // columnHeader3
-            // 
-            this.columnHeader3.Text = "Password";
-            // 
-            // columnHeader4
-            // 
-            this.columnHeader4.Text = "Email";
-            // 
-            // columnHeader5
-            // 
-            this.columnHeader5.Text = "Phonenumber";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(779, 390);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
@@ -203,7 +205,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonAlta);
             this.Name = "Form1";
             this.Text = "v";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -214,7 +216,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonAlta;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
--- Conexion/Conexion/Form1.cs
+++ Conexion/Conexion/Form1.cs
@@ -1,33 +1,18 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Windows.Forms;
 
 namespace Conexion
 {
     public partial class Form1 : Form
     {
-        NpgsqlConnection conexión = new NpgsqlConnectionNpgsqlConnection();
+
         string cadenaConexion;
         public Form1()
         {
             InitializeComponent();
-            cadenaConexion = " Server =  127.0.0.1; Port=5432; Database=parkboat;";
-            cadenaConexion += " User Id =admin;"; 
-            cadenaConexion += "; Password = lavall2c";
-            Conexion.ConnectionString = cadenaConexion;
-            try
-            {
-                Conexion.Open();
-            }
-            catch
-            {
-                MessageBox.Show("User or parssword incorrect");
-                Conexion.Close();
-            }
-            if (Conexion.state.ToString() == "open")
-            {
-                MessageBox.Show("Connection relized succesfuly");
-            }
         }
+
         private void label1_Click(object sender, EventArgs e)
         {
 
@@ -50,6 +35,74 @@ namespace Conexion
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            using (NpgsqlConnection conexion = new NpgsqlConnection())
+            {
+                cadenaConexion = " Server =  127.0.0.1; Port=5432; Database=parkboat;";
+                cadenaConexion += " User Id =postgres; ";
+                cadenaConexion += "; Password = lavall2c";
+                conexion.ConnectionString = cadenaConexion;
+                try
+                {
+                    conexion.Open();
+                }
+                catch
+                {
+                    MessageBox.Show("User or parssword incorrect");
+                    conexion.Close();
+                }
+                if (conexion.State.ToString() == "open")
+                {
+                    MessageBox.Show("Connection relized succesfuly");
+                }
+            }
+        }
+
+        private void buttonAlta_Click(object sender, EventArgs e)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            string cadenaComando;
+            cadenaComando = "INSERT INTO parkboat.userregistration (id,username,password,email,phonenumber)";
+            cadenaComando += "VALUES(@id, @username, @password,@email, @phonenumber);";
+            cmd.CommandText = cadenaComando;
+            cmd.Parameters.Add("@id", NpgsqlTypes.NpgsqlDbType.Varchar, 20).Value = TextBoxid.Text;
+            cmd.Parameters.Add("@username",NpgsqlTypes.NpgsqlDbType.Varchar, 100).Value = TextBoxusername.Text;
+            cmd.Parameters.Add("@password", NpgsqlTypes.NpgsqlDbType.Varchar, 100).Value = TextBoxpassword.Text;
+            cmd.Parameters.Add("@email", NpgsqlTypes.NpgsqlDbType.Varchar, 100).Value = TextBoxemail.Text;
+            cmd.Parameters.Add("@phonenumber", NpgsqlTypes.NpgsqlDbType.Varchar, 100).Value = TextBoxphonenumber.Text;
+
+            int res = cmd.ExecuteNonQuery();
+            if (res > 0)
+                MessageBox.Show("Information correctly upload");
+            else
+                MessageBox.Show("Information not uploaded to DB");
+            cmd.Parameters.Clear();
+            textBoxid.Text = "";
+            textBoxusername.Text = "";
+            textBoxpassword.Text = "";
+            textBoxemail.Text = "";
+            textBoxphonenumber.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            String sql = ("SELECT *FROM userregistration ORDER BY ID");
+            using (NpgsqlComand comand = new NpgsqlCommand(sql, conexion));
+            {
+                NpgsqlDataReader reader = comand.ExecuteReader();
+                int 1 = 0;
+                while (reader.Read())
+                {
+                    listView1.Items.Add(reader[0].ToString());
+                    listView1.Items.[1].SubItems.Add(reader[1].ToString());
+                    listView1.Items.[1].SubItems.Add(reader[2].ToString());
+                    listView1.Items.[1].SubItems.Add(reader[3].ToString());
+                    listView1.Items.[1].SubItems.Add(reader[4].ToString());
+                    listView1.Update();
+                    1++;
+                }
+                reader.Close();
+            }
 
         }
     }
